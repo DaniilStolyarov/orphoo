358864313716df3e0dafe0091ab592965cc0a7c9
Danst <danstolyarov79@gmail.com>
2022-06-24 15:11:48 +0300

    init

--- build/js/chat.js
+++ build/js/chat.js
@@ -1,6 +1,11 @@
-const socket = new WebSocket(location.origin.replace(/^http/, 'ws'))
+var socket = new WebSocket(location.origin.replace(/^http/, 'ws'))
 socket.onmessage = (msg) =>
 {
+    if (msg.data == 'pong')
+    {
+        console.log(msg.data)
+        return
+    }
     const author = document.createElement('div')
     author.className = 'author'
     const data = document.createElement('div')
@@ -13,6 +18,9 @@ socket.onmessage = (msg) =>
     message.append(data)
     document.querySelector('.view').prepend(message)
 }
+setInterval(() => {
+    socket.send('ping')
+}, 10000);
 [...document.getElementsByClassName('send')].forEach(send => 
     {
         const btn = send.querySelector('.msg-submit')
@@ -31,6 +39,24 @@ function submit(target)
 {
     const send = target.closest('.send')
     const field = send.querySelector('.msg-field')
-    socket.send(field.value)
+
+    if (socket.readyState == socket.CLOSED)
+    {
+        console.log('reopening')
+        socket.close()
+        socket = new WebSocket(location.origin.replace(/^http/, 'ws'))
+    }
+    function Send()
+    {
+        socket.send(field.value)
+    }
+    try
+    {
+        Send()
+    }
+    catch(err)
+    {
+        setTimeout(Send, 5000)
+    }
     field.value = ''
 }
