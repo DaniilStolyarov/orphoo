56eaf8186e484b11896bc71da359433626f4af17
Danst <danstolyarov79@gmail.com>
2022-06-24 15:08:57 +0300

    init

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {Server} = require('ws')
 const app = express()
-app.use(express.static('./orpho-react/build'))
+app.use(express.static('./build'))
 const http = require('http')
 const server = http.createServer(app)
 const wsServer = new Server({server})
