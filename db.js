fad6242f0c73aba74709cf9340d01697f010e1fa
danya <danstolyarov79@gmail.com>
2023-09-18 18:36:27 +0300

    new database

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const pg = require('pg')
 const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://nohpvjmjvitunu:304d77236b3955fe2a8cc35bae344359191b572f44670f187555740017e98c54@ec2-34-247-172-149.eu-west-1.compute.amazonaws.com:5432/d99erk98709nhb' ,
     ssl: {
       rejectUnauthorized: false
-    }
+    },
+    host: "localhost",
+    user : "postgres",
+    password : "postgres",
+    database : "db",
+    port : 5432
   });
 client.connect()
 const db = 
